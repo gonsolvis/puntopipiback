28513bc1a6c858a619654cfd1b41c720005c7c89
Beartoe7 <64451243+Beartoe7@users.noreply.github.com>
2023-03-13 11:38:56 +0100

    ok

--- models/Toilet.model.js
+++ models/Toilet.model.js
@@ -3,10 +3,21 @@ const {Schema, model} = require("mongoose");
 const toiletSchema = new Schema({
     title: String,
     description: String,
+    address: {
+        type: String,
+        required: true
+    },
+    latitude: {
+        type: Number,
+    },
+    longitude: {
+        type: Number,
+    },
+
     rating: {
           type: Number,
           min: 1,
-          max: 10
+          max: 5
         },
     imageUrl: String,
     creator: { type: Schema.Types.ObjectId, ref: "User" },
